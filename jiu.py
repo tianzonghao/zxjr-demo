04cd13538ad93a34dd2f16f4add0726888163743
zt <zt@imzhongxin.com>
2018-10-22 14:58:05 +0800

    conflict

---
+++ jiu.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Filename: jiu.py
+# Author: TZH
+# Date: 2018-10-18
+
+for i in range(1,10):
+	for j in range(1,i+1):
+		print('{}x{}={}\t'.format(j,i,i*j),end='')
+	print()
+print('good good study, day day up')
